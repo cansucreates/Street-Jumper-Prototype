0912b8fced1deef8fba6d93dd53e61f875f1aef6
cansucreates <caysagdic@gmail.com>
2025-05-08 18:17:21 +0300

    Added destroying obstacle logic after it cross the left bound

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 20f; // The speed at which the object moves left
     private PlayerController playerControllerScript; // Reference to the PlayerController script
+    private float leftBound = -15; // The left boundary for the object
 
     void Start()
     {
@@ -16,6 +17,10 @@ public class MoveLeft : MonoBehaviour
         if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime); // Move the object left at the specified speed
+        }
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle")) // Check if the object has moved past the left boundary
+        {
+            Destroy(gameObject); // Destroy the object if it goes past the left boundary
         }
     }
 }
