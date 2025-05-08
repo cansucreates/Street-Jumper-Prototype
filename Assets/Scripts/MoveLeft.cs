5531c39b8f4fb1f1756de629519754f4c50cb291
cansucreates <caysagdic@gmail.com>
2025-05-08 18:13:29 +0300

    Stopped repeating background when the game is over

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 20f; // The speed at which the object moves left
+    private PlayerController playerControllerScript; // Reference to the PlayerController script
 
-    void Start() { }
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>(); // Find the PlayerController script attached to the Player object
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime); // Move the object left at the specified speed
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime); // Move the object left at the specified speed
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,12 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0); // Position where the obstacle will spawn
     private float startDelay = 2f; // Delay before the first obstacle spawns
     private float repeatRate = 2f; // Time interval between obstacle spawns
+    private PlayerController playerControllerScript; // Reference to the PlayerController script
 
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate); // Call SpawnObstacle method repeatedly with a delay
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>(); // Find the PlayerController script attached to the Player object
     }
 
     // Update is called once per frame
@@ -17,6 +19,9 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); // Instantiate the obstacle at the specified position and rotation
+        if (playerControllerScript.gameOver == false) // Check if the game is not over
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); // Instantiate the obstacle at the specified position and rotation
+        }
     }
 }
