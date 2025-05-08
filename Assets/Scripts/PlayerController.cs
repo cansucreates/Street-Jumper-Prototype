b15bfa621f4ae93e7445660d0003e1e91d0ea48b
cansucreates <caysagdic@gmail.com>
2025-05-08 21:27:55 +0300

    Added player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,23 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier; // The gravity modifier for the player
     public bool isOnGround = true; // Check if the player is on the ground
     public bool gameOver; // Check if the game is over
+    private Animator playerAnim; // Reference to the Animator component
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>(); // Get the Animator component attached to the player
         Physics.gravity *= gravityModifier; // Adjust the gravity for the player
+        
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) // Check if the space key is pressed and the player is on the ground
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) // Check if the space key is pressed and the player is on the ground
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false; // Set isOnGround to false when the player jumps
+            playerAnim.SetTrigger("Jump_trig"); // Trigger the jump animation
         }
     }
 
@@ -33,6 +37,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true); // Trigger the death animation
+            playerAnim.SetInteger("DeathType_int", 1); // Set the death type to 1
         }
     }
 }
