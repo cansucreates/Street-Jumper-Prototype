6f5fdfe5397deab45c31b0d30fd5b5f73505885d
cansucreates <caysagdic@gmail.com>
2025-05-19 19:14:34 +0300

    Improvements and fixed some bugs + challenge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,7 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,22 +16,41 @@ public class PlayerController : MonoBehaviour
     public AudioClip jumpSound; // Reference to the jump sound effect
     public AudioClip crashSound; // Reference to the crash sound effect
     private AudioSource playerAudio; // Reference to the AudioSource component
+    public float jumpCooldown = 0.5f; // Cooldown time between jumps
+    private float nextJumpTime = 0f;
+    public GameObject UI;
+    public Button restartButton; // Reference to the start button
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload the current scene
+    }
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>(); // Get the Animator component attached to the player
+        playerAnim.applyRootMotion = false; // Disable root motion for the player to prevent unwanted movement
         playerAudio = GetComponent<AudioSource>(); // Get the AudioSource component attached to the player
-        Physics.gravity *= gravityModifier; // Adjust the gravity for the player
-        
+        Physics.gravity = new Vector3(0, -9.81f * gravityModifier, 0); // Set the gravity modifier for the player
+        UI.SetActive(false); // Deactivate the UI at the start
+
+        // Fix character position by freezing Z movement
+        playerRb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) // Check if the space key is pressed and the player is on the ground
+        if (Time.time > nextJumpTime && Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            playerRb.linearVelocity = new Vector3(0, playerRb.linearVelocity.y, 0); // Reset the x and z velocity to 0
+
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false; // Set isOnGround to false when the player jumps
+
+            // locking the future jump
+            nextJumpTime = Time.time + jumpCooldown;
+
             playerAnim.SetTrigger("Jump_trig"); // Trigger the jump animation
             dirtParticle.Stop(); // Stop the dirt particle effect when jumping
             playerAudio.PlayOneShot(jumpSound, 1.0f); // Play the jump sound effect
@@ -37,14 +59,18 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground")) // Check if the player collides with the ground
         {
             isOnGround = true; // Set isOnGround to true when the player collides with the ground
-            dirtParticle.Play(); // Play the dirt particle effect
+            if (!gameOver)
+            {
+                dirtParticle.Play(); // Play the dirt particle effect
+            }
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
+            StartCoroutine(ShowGameOverUI()); // Activate the UI when the game is over
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true); // Trigger the death animation
             playerAnim.SetInteger("DeathType_int", 1); // Set the death type to 1
@@ -53,4 +79,11 @@ public class PlayerController : MonoBehaviour
             playerAudio.PlayOneShot(crashSound, 1.0f); // Play the crash sound effect
         }
     }
+
+    private IEnumerator ShowGameOverUI()
+    {
+        yield return new WaitForSeconds(2f); // Wait for 2 second before showing the UI
+        UI.SetActive(true); // Activate the UI
+    }
 }
+
